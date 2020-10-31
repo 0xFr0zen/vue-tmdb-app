11040b16d22973f9c24317d8951a5571fc605b79
0xFr0zen <0xfr0zen@gmail.com>
2020-10-31 19:47:56 +0100

    first commit

---
+++ src/components/Base.vue
@@ -0,0 +1,40 @@
+<template>
+  <h1>sdas</h1>
+  <div class="genre" @click="setFilter('with_genre', 27)" />
+  <button value="LOL" @click="getResults">GET RESULTS</button>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Base',
+  data() {
+    return {
+      params: {
+        api_key: 'API_KEY_HERE'
+      },
+      URL: 'https://api.themoviedb.org/3/discover/movie',
+      result: {}
+    };
+  },
+  methods: {
+    setFilter(name, value) {
+      this.params[name] = value;
+    },
+    getResults() {
+      axios.get(this.URL, { params: this.params }).then(response => {
+        this.result = response.data.results;
+        console.log(this.result);
+      });
+    }
+  }
+};
+</script>
+<style scoped>
+.genre {
+  width: 2rem;
+  height: 2rem;
+  background-color: red;
+}
+</style>
